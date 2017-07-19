12f393534267725c2abe1c9016a3a9c7aa0f433a
Sam Corin <samcorin@gmail.com>
2017-07-19 19:53:18 +0900

    minor fix"

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/build'));
 
 app.all('*', function(req, res) {
   // failed to decode para %....% in index.html
-  res.set('Cache-Control', 'public, max-age=31536000');
+  // res.set('Cache-Control', 'public, max-age=31536000');
   res.sendFile(__dirname + '/build/index.html');
 });
 
