c6e0fb6e0d7cf1d227f42432d8cec46a87dea60f
Sam Corin <samcorin@gmail.com>
2017-07-19 18:31:52 +0900

    minor fixes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('build'))
+app.use(express.static(__dirname + '/build'));
 
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
+app.all('*', function(req, res) {
+  // failed to decode para %....% in index.html
+  res.set('Cache-Control', 'public, max-age=31536000');
+  res.sendFile(__dirname + '/build/index.html');
 });
 
-app.listen(3000, '127.0.0.1')
+var server = require('http').Server(app);
+server.listen(3000)
