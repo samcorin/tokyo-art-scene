86072195f48b2eb90b6ce24734c41b6772dcdbdb
Sam Corin <samcorin@gmail.com>
2017-07-19 16:33:04 +0900

    basic layout

--- result.js
+++ result.js
@@ -1,25 +1,25 @@
-// BW:
-// blur op2
-
-const colorArray = ['bg-aqua', 'bg-yellow', 'bg-orange'];
+const colorArray = ['bg-aqua', 'bg-yellow', 'bg-orange', 'bg-pink'];
 
 export const Result = ({ result }) => {
-	
-	const randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
-	
+	// const randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+		// <div class={`overlay ${randomColor}`}>
 	return (
 		<div class="result">
-			<div class={`overlay ${randomColor}`}>
+			<div class="overlay blur">
 				<img class="cover-img" src={result.thumbnail} />
 			</div>
 			<div class="text">
-				<p>hello</p>
+				<h2 class="text-blocky1">{result.eventName}</h2>
+				<p class="text-blocky2">{result.venueName}</p>
+				<div class="details text-blocky2">
+					<div class="info">
+						<p>Ends {result.dateEnd}</p>
+					</div>
+					 <div class="where">
+						<p>{result.area}</p>
+					</div> 
+				</div>
 			</div>
-
-			{/* <div class="tint"></div> */}
-			{/* <a href={result.html_url} class="blur" target="_blank">
-				{result.full_name}
-			</a> */}
 		</div>
-	)
+	);
 };
